845d83a7fb134b6f106391dcd7b667b6989c240b
Mbalenh <3408534@myuwc.ac.za>
2022-09-07 17:19:06 +0200

    added files

--- sql/DbFuction.js
+++ sql/DbFuction.js
@@ -1,51 +1,34 @@
 const DbFunction = (db) =>{
 	//insert usernames to table names.
-	const greets = async (name) =>{
+	const insertUsername = async (name) =>{
 		const result = await nameGreeted(name)
 		if(result === false){
-			 await db.none('INSERT INTO GreetingNames (username, counter) VALUES ($1,$2);', [name,1])
+			 await db.none('INSERT INTO totalphoneBill (username, price_plan) VALUES ($1,$2);', [name])
 		}
 		else {
-			let counter_net = await db.oneOrNone('SELECT counter FROM GreetingNames where username = $1',[name])
-			await db.none('UPDATE GreetingNames SET counter = counter + 1 WHERE username = $1', [name])
-		}
+			 await db.oneOrNone('SELECt price_plab FROM totalphoneBill where username = $1',[name])
+			
 	}
 
 	// check if the user already greeted
 	
-	const nameGreeted = async (name) => {
-        const result = await db.oneOrNone('SELECT username FROM GreetingNames WHERE username = $1', [name])
+	const pricePlanupdate = async (name) => {
+        const result = await db.oneOrNone('SELECT username FROM totalphoneBill WHERE username = $1', [name])
         return result !== null
     }
     //get all GreetingNames in th table
-    const getNames= async()=>{
-    	const result = await db.manyOrNone('SELECT * FROM GreetingNames')
+    const getPlans= async()=>{
+    	const result = await db.manyOrNone('SELECT * FROM totalphoneBill')
         return result
     }
 
 
-    // count how many time a user has been greeted
-    const getUserCounter = async (counter) => {
-        const result = await db.one('SELECT counter FROM GreetingNames WHERE username = $1', [counter])
-        return result.counter
-
-    }
-    //get counter for all users
-      const getCounter = async () => {
-        const result = await db.one('SELECT count(*) FROM GreetingNames')
-        return result.count
-    }
-    //clear all names in the table
- const clearNames = async () => {
-        await db.none('Delete FROM GreetingNames')
-    }
     return{
-    	greets,
-    	nameGreeted,
-    	getCounter,
-    	clearNames,
-    	getNames,
-    	getUserCounter
+    	insertUsername,
+        pricePlanupdate,
+        getPlans
+
+
     }
 }
 module.exports = DbFunction
